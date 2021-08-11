4cb583eae03e98887f3bef4e99abd4de5b714303
Pablo <pablochavezing@gmail.com>
2021-08-10 19:17:17 -0700

    references user and order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_021149) do
+ActiveRecord::Schema.define(version: 2021_08_11_021454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2021_08_11_021149) do
     t.bigint "product_id", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "number"
+    t.decimal "total"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -60,4 +70,5 @@ ActiveRecord::Schema.define(version: 2021_08_11_021149) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
